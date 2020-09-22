c564b0ed60e25f3bbc5f320f14a47143c033357e
MajorHorse <mylove103@foxmail.com>
2020-09-22 19:16:01 +0800

    八皇后问题

---
+++ src/leetcode/MyEightQueens.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * @author Major Tom
+ * @date 2020/9/22 19:05
+ * @description 八皇后问题
+ */
+public class MyEightQueens {
+    /**定义皇后的个数*/
+    int max=8;
+    /**定义棋盘*/
+    boolean[][] flag=new boolean[max][max];
+    /**标识行*/
+    boolean[] row=new boolean[max];
+    /**标识列*/
+    boolean[] col=new boolean[max];
+    /**解法总数*/
+    static int count=0;
+
+    public static void main(String[] args) {
+
+    }
+}
