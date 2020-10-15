2306d4383f0cbe16726feed6e3722ea3e0484a4f
MajorHorse <mylove103@foxmail.com>
2020-10-15 21:25:01 +0800

    解数独

--- src/leetcode/SolveSudoku.java
+++ src/leetcode/SolveSudoku.java
@@ -1,7 +1,5 @@
 package leetcode;
 
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * @author Major Tom
@@ -10,62 +8,120 @@ import java.util.List;
  */
 public class SolveSudoku {
 
-    public static void main(String[] args) {
-        /*char[][] chars=null;
-        solveSudoku(chars);*/
-        SolveSudoku ss=new SolveSudoku();
-        System.out.println(ss.permute(new int[]{1,2,3}).toString());
-    }
-
     /**
      * 解数独
-     * @param chars
+     * @param board
      */
-    public void solveSudoku(char[][] chars) {
-
-    }
-
-    /**
-     * 全排列问题：给出一串不重复的数字，求出此串数字的所有全排列组合
-     * @param nums 给定的数字
-     * @return 所有可能的全排列组合
-     *          假设数字为[1,2,3]，我们相求出它的全排列，求解过程大致可以用下图来表述
-     *                   1              2               3
-     *              2         3    1          3     1       2
-     *          3               2 3              1 2          1
-     *
-     *          以上求解过程本质上就是构造了一棵求解树，然后通过BST算法遍历树的每一条路径
-     *
-     */
-    /**存放最终求解的结果*/
-    List<List<Integer>> res=new LinkedList<>();
-    /**每次遍历出的树的路径*/
-    LinkedList<Integer> track=new LinkedList<>();
-    public List<List<Integer>> permute(int[] nums){
-        backTrack(nums);
-        return res;
-    }
-
-    /**
-     * 求解的核心算法：回溯法
-     * @param nums
-     */
-    public void backTrack(int[] nums) {
-        //跳出条件
-        if(track.size()== nums.length){
-            res.add(new LinkedList<>(track));
-            return;
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if(track.contains(nums[i])){
-                continue;
+    public void solveSudoku(char[][] board) {
+        /**判断某行里面某位数字是否已被摆放,是用格子的下标直接代表当前数字*/
+        boolean[][] rows=new boolean[9][9];
+        /**判断某列里面某位数字是否已被摆放*/
+        boolean[][] cols=new boolean[9][9];
+        /**判断某个3x3小块里面某数字是否已被摆放*/
+        boolean[][] blocks=new boolean[9][9];
+        //对字符出租进行初步处理
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j]!='.'){
+                    //对所有已经有数字的格子进行标识，即把格子内的数字对应位置标为true
+                    //比如此刻i=3,j=2,board[3][2]='4',则num=board[3][2]-‘1’=3
+                    int num=board[i][j]-'1';
+                    //表示第i+1列数字num-1已存在（由于数组是从0开始的，所以本质上其实是计算的0-8）
+                    rows[i][num]=true;
+                    cols[i][num]=true;
+                    //取整（5/3 * 3 + 5/3=5）。9x9的数独，3x3的block一共有9个，所以这样做其实是一个切分
+                    blocks[i / 3 * 3 + j / 3][num] = true;
+                }
             }
-            track.add(nums[i]);
-            backTrack(nums);
-            System.out.println(track.toString());
-            //这是回溯的关键步骤：状态恢复,即恢复到上一步的状态  
-            track.removeLast();
-            System.out.println(track.toString());
+        }
+        dfs(board,rows,cols,blocks,0,0);
+    }
+
+    private boolean dfs(char[][] board,boolean[][] rows,boolean[][] cols,boolean[][] blocks,int row, int col) {
+        //寻找每一个未被填上数字的空位置
+        while (board[row][col]!='.'){
+            //row和col的最大值均为8,这里是先行后列的扫描法
+            if (++col>=9){
+                row++;
+                col=0;
+            }
+            //满足此条件，说明已全部扫描完毕
+            if (row>=9){
+                return true;
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            //定位到分割为3x3的小块后的第几个块
+            int blockIndex=row/3*3+col/3;
+            //判断，即同一行&同一列&同一小块内都不能出现相同数字
+            //把0-8依次往格子里丢，看究竟哪个满足条件
+            if (!rows[row][i]&&!cols[col][i]&&!blocks[blockIndex][i]){
+                //+1是因为数组下标是从0开始的
+                board[row][col]=(char)('1'+i);
+                //行、列、块依次标记
+                rows[row][i]=true;
+                cols[col][i]=true;
+                blocks[blockIndex][i]=true;
+                //递归
+                if (dfs(board,rows,cols,blocks,row,col)){
+                    return true;
+                }else {
+                    //不满足条件，回溯
+                    rows[row][i]=false;
+                    cols[col][i]=false;
+                    blocks[blockIndex][i]=false;
+                    board[row][col]='.';
+                }
+            }
+        }
+        return false;
+    }
+
+    private void printSudoku(char[][] chars){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(chars[i][j]+" ");
+            }
+            System.out.println();
         }
     }
+
+    public static void main(String[] args) {
+        char[][] board = new char[][]{
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        SolveSudoku sudoku=new SolveSudoku( );
+        System.out.println(board);
+        sudoku.solveSudoku(board);
+        sudoku.printSudoku(board);
+    }
+    /**
+     * 输出：
+     * 5 3 . . 7 . . . .
+     * 6 . . 1 9 5 . . .
+     * . 9 8 . . . . 6 .
+     * 8 . . . 6 . . . 3
+     * 4 . . 8 . 3 . . 1
+     * 7 . . . 2 . . . 6
+     * . 6 . . . . 2 8 .
+     * . . . 4 1 9 . . 5
+     * . . . . 8 . . 7 9
+     * 5 3 4 6 7 8 9 1 2
+     * 6 7 2 1 9 5 3 4 8
+     * 1 9 8 3 4 2 5 6 7
+     * 8 5 9 7 6 1 4 2 3
+     * 4 2 6 8 5 3 7 9 1
+     * 7 1 3 9 2 4 8 5 6
+     * 9 6 1 5 3 7 2 8 4
+     * 2 8 7 4 1 9 6 3 5
+     * 3 4 5 2 8 6 1 7 9
+     */
 }
