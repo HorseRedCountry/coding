4202fe4f1359bf52504295f3bea1957d3df9dd35
MajorHorse <mylove103@foxmail.com>
2020-11-10 22:00:15 +0800

    下一个排列

---
+++ src/leetcode/NextPermutation.java
@@ -0,0 +1,51 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author Major Tom
+ * @date 2020/11/10 13:15
+ * @description 下一个排列
+ */
+public class NextPermutation {
+    public static void main(String[] args) {
+        int[] arr=new int[]{1,2,3};
+        nextPermutation(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+     * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+     * @param nums 初始数组
+     */
+    private static void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
